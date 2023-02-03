840bc8915dc0b40a228d6cd97ed55a526bb16e44
Dmitriy <112973042+bdvbuild@users.noreply.github.com>
2023-02-03 22:13:29 +0400

    Add files via upload
    Library added

---
+++ RevitApiTrainingLibrary/AllElementSelection.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitApiTrainingLibrary
+{
+    public class AllElementSelection
+    {
+
+        public static List<Pipe> GetAllPipes(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Pipe> pipes = new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_PipeCurves)
+                    .WhereElementIsNotElementType()
+                    .Cast<Pipe>()
+                    .ToList();
+
+            return pipes;
+        }
+
+        public static List<Wall> GetAllWalls(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Wall> walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            return walls;
+        }
+        public static List<FamilyInstance> GetAllDoors(ExternalCommandData commandData)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> doors = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Doors)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            return doors;
+        }
+    }
+}
---
+++ RevitApiTrainingLibrary/SelectionUtils.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitApiTrainingLibrary
+{
+    public class SelectionUtils
+    {
+        public static Element PickObject(ExternalCommandData commandData, string message = "Выберите элемент")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, message);
+            var oElement = doc.GetElement(selectedObject);
+            return oElement;
+        }
+
+        public static List<Element> PickObjects(ExternalCommandData commandData, string message = "Выберите элементы")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Element> elementList = new List<Element>();
+            try
+            {
+                var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, message);
+                elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            }
+            catch
+            {
+                TaskDialog.Show("Selection", "Работа прервана");
+            }
+                return elementList;
+        }
+    }
+}
